ca29f714bc9f58da2449c71ff29d46621198f7da
Chuck439 <firmankediri123@gmail.com>
2023-09-05 14:01:18 +0700

    Initial commit
    Signed-off-by: Chuck439 <firmankediri123@gmail.com>

---
+++ MyFirstJava16.java
@@ -0,0 +1,10 @@
+public class MyFirstJava16 {
+    public static void main(String[] args) {
+        System.out.println("***** IDENTITAS DIRI *****");
+        System.out.println("Nama            : M. Firmansyah");
+        System.out.println("Absen           : 16");
+        System.out.println("Kelas           : 1D");
+        System.out.println("Prodi           : D-IV T. Informatika");
+        System.out.println("Jurusan         : Teknologi Informasi");
+    }
+}
