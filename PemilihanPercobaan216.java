6654f6eb0413616d61634f76b7914aded06699d8
Chuck439 <firmankediri123@gmail.com>
2023-09-27 13:34:11 +0700

    PemilihanPercobaan216: Add condition for total
    Signed-off-by: Chuck439 <firmankediri123@gmail.com>

--- PemilihanPercobaan216.java
+++ PemilihanPercobaan216.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class PemilihanPercobaan216 {
     public static void main(String[] args) {
         Scanner input16 = new Scanner(System.in);
+        String message = "";
 
         System.out.print("Nilai uas     : ");
         float uas = input16.nextFloat();
@@ -12,9 +13,24 @@ public class PemilihanPercobaan216 {
         float kuis = input16.nextFloat();
         System.out.print("Nilai tugas   : ");
         float tugas = input16.nextFloat();
-
         float total = (uas*0.4F) + (uts*0.3F) + (kuis*0.1F) + (tugas*0.2F);
-        String message = total < 65 ? "Remidi" : "Tidak remidi";
-        System.out.println("Nilai akhir = " +total+ " sehingga" +message);
+
+        if (total <= 100 && total > 80) {
+            message = "A";
+        } else if (total <= 80 && total > 73) {
+            message = "B+";
+        } else if (total <= 73 && total > 65) {
+            message = "B";
+        } else if (total <= 65 && total > 60) {
+            message = "C+";
+        } else if (total <= 60 && total > 50) {
+            message = "C";
+        } else if (total <= 50 && total > 39) {
+            message = "D";
+        } else if (total <= 39) {
+            message = "E";
+        }
+
+        System.out.println("Nilai akhir = " +total+ " dan nilai huruf anda adalah " +message);
     }
 }
