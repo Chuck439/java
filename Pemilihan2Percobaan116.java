c8a5de92956432e426958316b048c37cba6e86d8
Firmank123 <firmankediri123@gmail.com>
2023-10-04 12:07:51 +0700

    Import Pemilihan2Percobaan116
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

---
+++ Pemilihan2Percobaan116.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Pemilihan2Percobaan116 {
+    
+    public static void main(String[] args) {
+        int tahun;
+        Scanner input16 = new Scanner(System.in);
+
+        // Input
+        System.out.print("Masukkan tahun: ");
+        tahun = input16.nextInt();
+
+        // Process
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0)
+                System.out.println("Tahun kabisat");
+        } else {
+            System.out.println("Bukan tahun kabisat");
+        }
+    }
+}
