b05cfcf00bf04d55bab31d88317f84c4885721d5
Firmank123 <firmankediri123@gmail.com>
2023-10-25 14:23:15 +0700

    Import ArrayBilangan16
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

---
+++ ArrayBilangan16.java
@@ -0,0 +1,20 @@
+package jobsheet9;
+
+public class ArrayBilangan16 {
+    public static void main(String[] args) {
+        // Var
+        int[] bil = new int[4];
+        
+        // Process
+        bil[0] = 5;
+        bil[1] = 13;
+        bil[2] = -7;
+        bil[3] = 17;
+
+        // Output
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+    }
+}
