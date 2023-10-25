30918d0ebfcad72576aad9979a66580995963f23
Firmank123 <firmankediri123@gmail.com>
2023-10-25 15:13:31 +0700

    ArrayBilangan16: Declare and initiate array element at once
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

--- ArrayBilangan16.java
+++ ArrayBilangan16.java
@@ -3,14 +3,8 @@ package jobsheet9;
 public class ArrayBilangan16 {
     public static void main(String[] args) {
         // Var
-        int[] bil = new int[4];
+        int[] bil = {5, 13, -7, 17};
         
-        // Process
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
-
         // Output
         System.out.println(bil[0]);
         System.out.println(bil[1]);
