f66fefec960a4b8c74eebebc7184f9962b682c5f
Firmank123 <firmankediri123@gmail.com>
2023-11-01 07:20:19 +0000

    Firmansyah1: Basic function
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

--- Firmansyah.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-
-public class Firmansyah {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-    }
-}
--- Firmansyah1.java
+++ Firmansyah1.java
@@ -3,5 +3,26 @@ import java.util.Scanner;
 public class Firmansyah {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        // Var
+        double bobotPotensiAkademik = 0.5;
+        double bobotBahasaInggris = 0.3;
+        double bobotWawancara = 0.2;
+
+        // Input
+        System.out.print("Masukkan nilai Tes Potensi Akademik (0-100): ");
+        double nilaiPotensiAkademik = input.nextDouble();
+
+        System.out.print("Masukkan nilai Tes Bahasa Inggris (0-100): ");
+        double nilaiBahasaInggris = input.nextDouble();
+
+        System.out.print("Masukkan nilai Tes Wawancara (0-100): ");
+        double nilaiWawancara = input.nextDouble();
+
+        // Process
+        double nilaiAkhir = (nilaiPotensiAkademik * bobotPotensiAkademik) + (nilaiBahasaInggris * bobotBahasaInggris) + (nilaiWawancara * bobotWawancara);
+
+        // Output
+        System.out.println("Nilai Akhir Anda Adalah: " + nilaiAkhir);
     }
 }
