40bee739551531b411b4380c20b435259818e195
Firmank123 <firmankediri123@gmail.com>
2023-11-22 13:37:10 +0700

    TerimaKasih_16: Add function UcapanTambahan
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

--- TerimaKasih_16.java
+++ TerimaKasih_16.java
@@ -4,7 +4,12 @@ public class TerimaKasih_16 {
         System.out.println("Thank you for being the best teacher in the world.\n"+
         "You inspired in me a love for learning and made me feel like I could ask you anyrhing,");
     }
+    public static void UcapanTambahan(String ucapan){
+        System.out.println(ucapan);
+    }
     public static void main(String[] args) {
         UcapanTerimaKasih();
+        String ucapan = "Terima kasih Pak.. Bu.. Semoga Sehat Selalu";
+        UcapanTambahan(ucapan);
     }
 }
