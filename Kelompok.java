9a5b7e04b60e6c904764507a1ab137591577a948
Firmank123 <firmankediri123@gmail.com>
2023-12-10 14:16:17 +0700

    Kelompok: Fix class name
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

--- Kelompok.java
+++ Kelompok.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class cobacoba {
+public class Kelompok {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int pilihan;
